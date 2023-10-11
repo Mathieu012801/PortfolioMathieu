4148229ae0549f3196f0415f914587387cd4c289
Mathieu Delcourt <mathieudelcourt@macbook-air-de-mathieu.home>
2023-10-11 17:14:23 +0200

    resize cards

--- src/Data/Projet.js
+++ src/Data/Projet.js
@@ -1,6 +1,5 @@
 import Booki from '../assets/projets/mockup_bookifin (1).webp';
 import OMF from '../assets/projets/ohmyfood-mockup.webp';
-import print from '../assets/projets/print-it.PNG';
 import Kasa from '../assets/projets/kasa-mockup.webp';
 import Events from '../assets/projets/77events.webp'
 const Portfolio = [
@@ -32,13 +31,6 @@ const Portfolio = [
     description: " site d'une agence immobilière",
     image: Kasa,
     url : "https://github.com/Mathieu012801/Kasa.git"
-  },
-  {
-    id: "5",
-    titre: "Print-int",
-    description: "site d'une imprimerie",
-    image: print,
-    url: "https://ocrp-5-mathieu012801.vercel.app/"
   },
 
 ];
