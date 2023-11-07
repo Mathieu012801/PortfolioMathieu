9f56692474d63cf87bad137744418f74b370ef43
Mathieu Delcourt <mathieudelcourt@macbook-air-de-mathieu.home>
2023-11-07 13:04:19 +0100

    peaufinage2.0

--- src/components/hero/Math.jsx
+++ src/components/hero/Math.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className="hero">
       <h1>Bienvenue sur mon portfolio</h1>
-      <p>Je suis Mathieu Delcourt & je suis développeur Front-end</p>
+      <p>Mathieu Delcourt je suis développeur Front-end</p>
       <div>
         <a href={CV} aria-label="Télécharger mon CV au format PDF">
           <button className='hero__button'>
